2234ccd687ad184e0515f4a6d776d07c49d00444
Швеенков Борис <shveenkov@asus-shveenkov.home>
2014-07-24 23:51:17 +0400

    Initilization

---
+++ protected/controllers/MemberController.php
@@ -0,0 +1,64 @@
+<?php
+
+class MemberController extends Controller
+{
+    
+    public function init()
+    {
+	$this->defaultAction = 'profile';
+        parent::init();
+    }
+    
+    
+	public function actionEndregistration()
+	{
+		$this->render('endregistration');
+	}
+
+	public function actionLogin()
+	{
+		$this->render('login');
+	}
+
+	public function actionLogout()
+	{
+		$this->render('logout');
+	}
+
+	public function actionProfile()
+	{
+		$this->render('profile');
+	}
+
+	public function actionRegestration()
+	{
+		$this->render('regestration');
+	}
+
+	// Uncomment the following methods and override them if needed
+	/*
+	public function filters()
+	{
+		// return the filter configuration for this controller, e.g.:
+		return array(
+			'inlineFilterName',
+			array(
+				'class'=>'path.to.FilterClass',
+				'propertyName'=>'propertyValue',
+			),
+		);
+	}
+
+	public function actions()
+	{
+		// return external action classes, e.g.:
+		return array(
+			'action1'=>'path.to.ActionClass',
+			'action2'=>array(
+				'class'=>'path.to.AnotherActionClass',
+				'propertyName'=>'propertyValue',
+			),
+		);
+	}
+	*/
+}
---
+++ protected/views/member/profile.php
@@ -0,0 +1,16 @@
+<?php
+/* @var $this MemberController */
+
+$this->breadcrumbs=array(
+	Yii::app()->user->name=>array('/member'),
+	'Профиль пользователя',
+);
+?>
+<h1>Профиль пользователя</h1>
+
+<p>
+    Здесь должен быть виджет вкладок профиля пользователя
+</p><p>
+	You may change the content of this page by modifying
+	the file <tt><?php echo __FILE__; ?></tt>.
+</p>
