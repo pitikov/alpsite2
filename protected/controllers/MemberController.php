1a06ca0e63568413132790a4273511621c3cfb29
Питиков Е.А <pitikov@yandex.ru>
2014-07-30 18:04:39 +0400

    Выставил права доступа на MemberController

--- protected/controllers/MemberController.php
+++ protected/controllers/MemberController.php
@@ -198,30 +198,43 @@ class MemberController extends Controller
 	    $this->render('federationprofile');
 	}
 
-	// Uncomment the following methods and override them if needed
-	/*
 	public function filters()
 	{
-		// return the filter configuration for this controller, e.g.:
-		return array(
-			'inlineFilterName',
-			array(
-				'class'=>'path.to.FilterClass',
-				'propertyName'=>'propertyValue',
-			),
-		);
+	    return array(
+		'accessControl'
+	    );
 	}
-
-	public function actions()
+	
+	
+	public function accessRules()
 	{
-		// return external action classes, e.g.:
-		return array(
-			'action1'=>'path.to.ActionClass',
-			'action2'=>array(
-				'class'=>'path.to.AnotherActionClass',
-				'propertyName'=>'propertyValue',
-			),
-		);
+	    return array(
+	        array(
+		    'allow',
+		    'actions'=>array('federationprofile',),
+		    'roles'=>array('fapo'),
+		),
+		array(
+		    'deny',
+		    'actions'=>array('login','registration','endregistration','pwdrestore',),
+		    'roles'=>array('user'),
+		),
+		array(
+		    'allow',
+		    'actions'=>array('articles','mail','logout','profile','peaklist'),
+		    'roles'=>array('user'),
+		),
+		array(
+		    'deny',
+		    'actions'=>array('articles','mail','logout','profile','peaklist','federationprofile'),
+		    'roles'=>array('guest'),
+		),
+		array(
+		    'allow',
+		    'actions'=>array('login','registration','endregistration','pwdrestore'),
+		    'roles'=>array('guest'),
+		),
+	    );
 	}
-	*/
+	
 }
