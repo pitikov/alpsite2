f4e30805dd7985cfdbfb33b11ae7bdf089cb438a
Питиков Е.А <pitikov@yandex.ru>
2014-07-31 08:39:40 +0400

    Исправления в части сообщений об ошибках

--- protected/models/PwdRestoreForm.php
+++ protected/models/PwdRestoreForm.php
@@ -6,7 +6,7 @@
         {
 	    return array(
 	        array('user', 'required', 'message'=>'Введите Login или E-Mail зарегистрированного пользователя для восстановления пароля'),
-	        array('user', 'isexists', 'message'=>'Пользователь с указанными данными не найден'),
+	        array('user', 'isexists'),
 	    );
         }
         
@@ -19,9 +19,11 @@
         
         public function isexists($attribute,$params)
         {
-	    if (strlen($this->user) > 0 ) {
-		$users = User::model()->findAll('`login`=:User or `email`=:User', array(':User'=>$this->user));
-		if (count($users) === 0) $this->addError('username','Пользователь с указанными данными не найден.');
+	    if (!$this->hasErrors()) {
+		$user = User::model()->find('`login`=:User or `email`=:User', array(':User'=>$this->user));
+		if (!isset($user->uid)) {
+		    $this->addError('username','Пользователь с указанными данными не найден.');
+		}
 	    }
         }
 
