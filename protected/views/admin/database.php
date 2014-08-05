9866ea434443c294f57225655659daf59756c58c
E.A.Pitikov <pitikov@yandex.ru>
2014-08-06 01:31:40 +0400

    Пример работы с картой (для разбора)

--- protected/views/admin/database.php
+++ protected/views/admin/database.php
@@ -12,3 +12,15 @@ $this->breadcrumbs=array(
 	You may change the content of this page by modifying
 	the file <tt><?php echo __FILE__; ?></tt>.
 </p>
+<?php
+$this->widget('ext.easymap.EasyMap', array(
+    'key' => '', /*Insert your developer API key for google map*/
+    'id' => 'newmap', /*This is the id of the map wrapper div*/
+    'latitude' => '44.572646000000000000', /*Place Latitude*/
+    'longitude' => '44.5763894999999950000', /*Place Longitude*/
+    'maptype' => 'HYBRID', /*ROADMAP, SATELITE, HYBRID, TERRAIN*/
+    'zoom' => '10', /*Zoom level. Default is 7*/
+    'width' => '700', /*Map height*/
+    'height' => '600', /*Map width*/
+    'markertitle' => '', /*Title of the place marker*/
+));?>
