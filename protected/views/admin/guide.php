9ce62efe1d29029af8c4e127347ff260abbdb98d
Питиков Е.А <pitikov@yandex.ru>
2014-08-05 09:29:06 +0400

    Набросок дерева классификатора

--- protected/views/admin/guide.php
+++ protected/views/admin/guide.php
@@ -3,4 +3,37 @@
     array_push($this->breadcrumbs, $pagename);
 ?>
 <h1><?php echo $pagename; ?></h1>
-На данной странице следует разместить редактор классификатора.
+<?php 
+  $this->widget('system.web.widgets.CTreeView', array(
+      'data'=>array(
+          'node1'=>array(
+	      'text'=>'Кавказ',
+	      'expanded'=>true,
+	      'children'=>array(
+		  array(
+		      'text'=>'Безенги',
+		      'expanded'=>false,
+		      'children'=>array(
+			  array(
+			      'text'=>'Архимед',
+			      'expanded'=>false,
+			      'children'=>array(
+				  array('text'=>'2А по С стене'),
+				  array('text'=>'3А по Ю-З склону'),
+				  array('text'=>'3Б Арбуз'),
+			      ),
+			  ),
+		      ),
+		  ),
+		  array(
+		      'text'=>'Цей',
+		  ),
+		  array(
+		      'text'=>'Уллутау',
+		  ),
+		  
+	      )
+	  ),
+      )
+  ));
+?>
