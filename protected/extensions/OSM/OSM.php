afab301c858fc043481290fd91a87867e8c72779
Питиков Е.А <pitikov@yandex.ru>
2014-08-08 10:33:18 +0400

    Добавил слой маршрутов

--- protected/extensions/OSM/OSM.php
+++ protected/extensions/OSM/OSM.php
@@ -30,10 +30,17 @@ class OSM extends CWidget {
     public $height=500;
     public $position=array('lon'=>5008810.59272, 'lat'=>7021711.41236);
     public $zoom=11;
+    public $findEngine=false;
 
     public function init()
-    {    
+    {   
+    if ($this->findEngine) {
 ?>
+<b>Поиск:</b><input type='text' value='Введите название и нажмите "ввод"'/>
+<div id='find_return'>
+<!-- Здесь разместить вывод результатов поиска -->
+</div>
+<?php } // end findEngine ?>
 <script src="http://openlayers.org/api/OpenLayers.js"></script>
 <?php if (isset($this->layers['google'])) { ?>
   <script src="http://maps.google.com/maps/api/js?v=3&amp;sensor=false"></script>
@@ -44,14 +51,14 @@ class OSM extends CWidget {
   var map = new OpenLayers.Map('<?php echo $this->id; ?>', { controls: [] });
 
   <?php if (isset($this->layers['osm'])) { ?>
-  var osm = new OpenLayers.Layer.OSM("OSM карта");
+  var osm = new OpenLayers.Layer.OSM("<?php if (isset($this->layers['osm']['title'])) echo $this->layers['osm']['title']; else echo "OSM карта"; ?>");
   map.addLayer(osm);
   <?php } ?>
 
 
   <?php if (isset($this->layers['google'])) { ?>
  
-  var gmap = new OpenLayers.Layer.Google("GoogleMaps",
+  var gmap = new OpenLayers.Layer.Google("<?php if (isset($this->layers['google']['title'])) echo $this->layers['google']['title']; else echo "GoogleMaps гибрид"; ?>",
                 {type: google.maps.MapTypeId.HYBRID, numZoomLevels: 20}
             );
   map.addLayer(gmap);
@@ -60,6 +67,13 @@ class OSM extends CWidget {
 
   var markers = new OpenLayers.Layer.Markers("Точки пользователя");
   map.addLayer(markers);
+  
+  <?php } ?>
+  
+  <?php if (isset($this->layers['route'])) { ?>
+
+  var routes = new OpenLayers.Layer.PointTrack("Маршруты");
+  map.addLayer(routes);
   
   <?php } ?>
     
@@ -90,10 +104,9 @@ class OSM extends CWidget {
   
   
 </script>
-	     
 <?php }
 // end of init()    
-    
+    /// @todo Здесь разместить вкладки точек пользователя и маршрута
 }
 
 ?>
