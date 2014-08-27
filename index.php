ab29d703aa0a3c21d4dc57a6a18dece01619d712
Питиков Е.А <pitikov@yandex.ru>
2014-08-27 08:21:19 +0400

    Автоматическое создание каталогов assets и protected/runtime на этапе исполнения

--- index.php
+++ index.php
@@ -9,5 +9,11 @@ defined('YII_DEBUG') or define('YII_DEBUG',true);
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
+$assets = dirname(__FILE__).'/assets';
+$runtime = dirname(__FILE__).'/protected/runtime';
+
+if (!is_dir($assets)) mkdir($assets);
+if (!is_dir($runtime)) mkdir($runtime);
+
 require_once($yii);
 Yii::createWebApplication($config)->run();
