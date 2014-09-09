9f42f617c55f58e04e5e82c55aa5d5ab2bc6d777
felis-linx <felis.linx@gmail.com>
2014-09-09 14:35:47 +0400

    Изменения по доктайпу -> HTML5 и по таблицам стилей

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -1,7 +1,12 @@
 <?php /* @var $this Controller */ ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ru" lang="ru">
+<!DOCTYPE html>
+<html lang="ru" xml:lang="ru">
+<head>
+<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+<meta http-equiv="content-language" content="ru" />
 <?php
+// @note используем доктайп HTML5 и статичную информацию выводим без использования PHP
+/*
 echo CHtml::tag('html', array(
     'xmlns'=>"http://www.w3.org/1999/xhtml",
     'xml:lang'=>"ru",
@@ -12,21 +17,48 @@ echo CHtml::tag('head',array(),
     CHtml::tag('meta', array('name'=>'language', 'content'=>'ru')).
     CHtml::tag('title', array(), CHtml::encode($this->pageTitle))
 );
+*/
+
+echo CHtml::tag('title', array(), CHtml::encode($this->pageTitle));
 
 $clientScript = Yii::app()->clientScript;
-$clientScript->registerCssFile(Yii::app()->request->baseUrl.'/css/screen.css', "screen, projection");
-$clientScript->registerCssFile(Yii::app()->request->baseUrl.'/css/print.css', 'print');
-/// @todo Подключение is.css только для ie младше 8
-//$clientScript->registerCssFile(Yii::app()->request->baseUrl.'/css/ie.css', "screen, projection");
-$clientScript->registerCssFile(Yii::app()->request->baseUrl.'/css/main.css');
-$clientScript->registerCssFile(Yii::app()->request->baseUrl.'/css/form.css');
-$clientScript->registerCssFile(Yii::app()->request->baseUrl.'/css/alpsite.css');
 
-echo CHtml::tag('body',array(),
-    CHtml::tag('div', array('class'=>'container', 'id'=>'page'),
+// @note Для осликов младше 9-ки добавляем элементы HTML5 для совместимости
+echo '
+<!-- [if lt IE 9]>
+<script>
+var e = ("abbr,article,aside,audio,canvas,datalist,details," +
+"figure,footer,header,hgroup,mark,menu,meter,nav,output," +
+"progress,section,time,video").split(',');
+for (var i = 0; i < e.length; i++) {
+document.createElement(e[i]);
+} </script>
+<![endif]-->'.PHP_EOL;
+
+// @todo Сделать CSS main - общие элементы стиля, desctop, mobile и print - верстка и т.п. специфичные для устройств
+echo '
+<link rel="stylesheet" type="text/css" href="/css/main.css" />
+<link rel="stylesheet" type="text/css" href="/css/desktop.css" media="only screen and (min-device-width: 1000px)" />
+<link rel="stylesheet" type="text/css" href="/css/mobile.css" media="only handheld, screen and (max-device-width: 480px)" />
+<link rel="stylesheet" type="text/css" href="/css/print.css" media="print" />
+<link rel="stylesheet" type="text/css" href="/css/form.css" />
+';
+
+// @note Подключение is.css только для ie младше 8 с использованием метаязыка браузера
+echo '
+<!-- [if lt IE 8]>'.PHP_EOL;
+//$clientScript->registerCssFile(Yii::app()->request->baseUrl.'/css/ie.css', "screen");
+echo '<![endif]-->'.PHP_EOL;
+
+?>
+</head>
+<body>
+<?php
+
+echo CHtml::tag('div', array('class'=>'container', 'id'=>'page'),
 	CHtml::tag('div', array('id'=>'header'), 
 	    CHtml::tag('div', array('id'=>'logo'), CHtml::encode(Yii::app()->name))
-	),false),false);
+	),false);
 	
 	echo CHtml::tag('div', array('id'=>'mainmenu'), null, false);
 	    $this->widget('zii.widgets.CMenu',array(
@@ -60,6 +92,9 @@ echo CHtml::tag('body',array(),
 	    'All Rights Reserved'.CHtml::tag('br').Yii::powered()
 	);
     echo CHtml::closeTag('div'); // div#page
-echo CHtml::closeTag('body');
-	
-echo CHtml::closeTag('html');
+
+//echo CHtml::closeTag('body');
+//echo CHtml::closeTag('html');
+?>
+</body>
+</html>
